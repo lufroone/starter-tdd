6e15ed9c68a616894aee161f4a35a431013a001f
Dimitri Merejkowsky <dimitri@dmerej.info>
2023-06-06 16:46:08 +0200

    Fix typo

--- ts/jest/answer.test.ts
+++ ts/jest/answer.test.ts
@@ -1,7 +1,7 @@
 import getAnswer from './answer'
 import { describe, expect, test } from '@jest/globals'
 
-describe('gettinng answers', () => {
+describe('getting answers', () => {
   test('it returns 42', () => {
     const res = getAnswer()
     expect(res).toBe(42)
